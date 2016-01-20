c56203be7fcb0c7e2c2bd94b44056d0d52171b16
Vanessa Ceballos <Vanessa@Vanessas-MacBook-Pro.local>
2016-01-19 18:34:54 -0800

    first commit

--- app/views/app.jsx
+++ app/views/app.jsx
@@ -1,18 +1,28 @@
 import React from 'react'
 import { render } from 'react-dom'
 import '~/app/assets/styles/app'
+import { Router, IndexRoute, Route, browserHistory } from 'react-router'
+
+import Home from './home/home'
+
 
 class App extends React.Component {
   render() {
-    const { children } = this.props
+    const { children } = this.props;
 
     return (
       <div className='container'>
-        Hello World
+          Hello World
         { children }
       </div>
     )
   }
 }
 
-render(<App />, document.getElementById('react'))
+render((
+    <Router history={ browserHistory }>
+        <Route path="/" component={ App }>
+            <IndexRoute component={ Home }/>
+        </Route>
+    </Router>
+), document.getElementById('react'));
