58c48ceca16485cbd20ead6149f2ecf5da1c05df
Vanessa Ceballos <Vanessa@Vanessas-MacBook-Pro.local>
2016-01-26 17:56:59 -0800

    gitignore

---
+++ node_modules/legit-tests/middleware/setState.js
@@ -0,0 +1,12 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = setState;
+
+function setState(state) {
+  this.instance.setState(state);
+}
+
+module.exports = exports["default"];
